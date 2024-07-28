e42a334a910d929215931c86aacab31609c681d9
shuxiangfan <linclonfan@outlook.com>
2024-07-28 23:51:14 +0800

    submodule: drivers: staging: Add wlan-qc
    Signed-off-by: shuxiangfan <linclonfan@outlook.com>

--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@
 args="-j$(nproc --all) \
 	O=out \
 	ARCH=arm64 \
-	CLANG_TRIPLE=aarch64-linux-gnu- \
-	CROSS_COMPILE=${HOME}/cbl/bin/aarch64-linux-gnu- \
-	CC=${HOME}/cbl/bin/clang \
-	CROSS_COMPILE_ARM32=${HOME}/cbl/bin/arm-linux-gnueabi- "
+	CROSS_COMPILE=aarch64-linux-gnu- \
+	CROSS_COMPILE_ARM32=arm-linux-gnueabi- "
 	make ${args} aosp_yoshino_maple_defconfig
 	make ${args}
