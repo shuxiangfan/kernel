4798f0b5b612b166e6d73cf3dd494cc7fe99dc60
shuxiangfan <linclonfan@outlook.com>
2024-08-17 20:53:43 +0800

    build: using cbl
    Signed-off-by: shuxiangfan <linclonfan@outlook.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 args="-j$(nproc --all) \
 	O=out \
 	ARCH=arm64 \
-	CROSS_COMPILE=aarch64-linux-gnu- \
-	CROSS_COMPILE_ARM32=arm-linux-gnueabi- "
+	CLANG_TRIPLE=aarch64-linux-gnu- \
+	CROSS_COMPILE=${HOME}/cbl/bin/aarch64-linux-gnu- \
+	CC=${HOME}/cbl/bin/clang \
+	CROSS_COMPILE_ARM32=${HOME}/cbl/bin/arm-linux-gnueabi- "
 	make ${args} aosp_yoshino_maple_defconfig
-	make ${args} -j16 CONFIG_DEBUG_SECTION_MISMATCH=y
+	make ${args}
